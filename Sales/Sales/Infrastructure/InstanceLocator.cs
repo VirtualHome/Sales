789942d37702773b74cdbdf25242b118e5a661c7
Andres Orozco (Home) <andres@virtual-home.co>
2018-08-26 12:00:37 -0500

    Products List

---
+++ Sales/Sales/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Infrastructure
+{
+    using Sales.ViewModels;
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
